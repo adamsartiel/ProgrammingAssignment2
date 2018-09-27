6cf8dad20d85034d88bfc34292380e91f4bafcdc
adamsartiel <43400311+adamsartiel@users.noreply.github.com>
2018-09-27 09:20:56 +0300

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,28 @@
 
 ## makeCachematrix receives the matrix x and makes it the global environment argument m. Then it creates the empty 
 ## matrix im.
+##
+## To test I have used m1<<-matrix(c(4,7,2,6),2,2), m2<-matrix(c(0.6,-0.2,-0.7,0,4),2,2) 
+## Then it runs like this:
+##> cacheSolve(m1)
+##     [,1] [,2]
+##[1,]  0.6 -0.7
+##[2,] -0.2  0.4
+##> cacheSolve(m1)
+##getting cached data
+##     [,1] [,2]
+##[1,]  0.6 -0.7
+##[2,] -0.2  0.4
+##> cacheSolve(m2)
+##     [,1] [,2]
+##[1,]    4    7
+##[2,]    2    6
+##> cacheSolve(m2)
+##getting cached data
+##     [,1] [,2]
+##[1,]    4    7
+##[2,]    2    6
+
 
 makeCacheMatrix <- function(x = matrix()) {
     ## Reset variables m, im, does not return any value
@@ -17,7 +39,7 @@ makeCacheMatrix <- function(x = matrix()) {
 ## if any of the conditions above is not met, then the functions calls makeCacheMatrix and solves m. It returns 
 ## im, the inverted matrix of x.
 
-cacheSolve <- function(x = matrix()) {
+cacheSolve <- function(x = matrix) {
         ## Return a matrix that is the inverse of 'x'
   if((!is.null(im)) && (x==m))
     {
